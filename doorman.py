b729332ca0bccf101a828376b895793d1fd867d9
Rodrigo Campos <rodrigo@webdevel.cl>
2017-05-31 19:57:02 -0400

    testing gpio

--- doorman.py
+++ doorman.py
@@ -2,9 +2,23 @@ import fileinput
 import requests
 import json
 
-url = "http://192.168.1.83:3000/visit"
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+
+#url = "http://xsknet.dyndns.org:82/visit"
+url = "http://192.168.1.66:3000/visit"
 
 headers = { 'Content-Type' : 'application/json' }
 
 for line in fileinput.input():
     response = requests.get(url, data=json.JSONEncoder().encode({"rut": line.rstrip() }), headers=headers)
+    GPIO.output(15, GPIO.HIGH)
+    GPIO.output(16, GPIO.HIGH)
+    sleep(5) 
+    GPIO.output(15, GPIO.LOW)
+    GPIO.output(16, GPIO.LOW)
